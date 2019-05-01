347b4886db65ee9882b35e59d02511c3309bcb4c
Erick Mathus <erick.mathus@yahoo.com>
2019-05-01 13:30:29 -0400

    added crow story

---
+++ main.js
@@ -0,0 +1,8 @@
+for(crowNum = 99; crowNum >= 1; crowNum--){
+  if (crowNum != 1){
+    console.log(crowNum + "crows on the wall" + crowNum + "crows." + "1 fell down and became a wight" + (crowNum - 1) + "crows are left")
+  } else {
+  console.log("1 single crown. It fell down and became a wight. There is no one left to defend Westeros")
+  }
+
+}
